041a7a956a7c816e9d0da1bdeda6ecafb4072b3b
Yogesh Chavan <ychavan@mdsol.com>
2020-03-27 00:06:07 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/main.scss';
 
 class App extends React.Component {
-    render() {
-        return (
-            <h1>Welcome to REACT!!</h1>
-        )
-    }
+  render() {
+    return <h1>Welcome to REACT!!</h1>;
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
 const path = require('path');
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'public')
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }]
-    },
-    mode: 'development',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
 };
